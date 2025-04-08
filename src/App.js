b4963de6734f2fba890a78ac747aab24fd086639
Camila Guillan <62452679+camiguillan@users.noreply.github.com>
2025-04-08 22:43:40 +0000

    creating a custom hook

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Navigation from "./components/Nav/Navigation";
 import ProductsPage from "./containers/Products";
 import FavoritesPage from "./containers/Favorites";
 
+//USE CONTEXT IS BETTER FOR DATA THAT IT IS NOT CHANGING ALL THE TIME -> LOW FRECUENCY 
+//CONTEXT API -> IT HAS NO WAY TO IDENTIFY WHICH COMPONENT IT IS CHANGING IS STATE
+// THAT IS WHY ALL THE COMPONENTS ARE RE RENDERED IF AT LIST THE STATE OF ONE IS CHANGED
+// THIS IS THE PARTH THAT IMPACTS PERFORMANCE
+
+
 const App = (props) => {
   return (
     <React.Fragment>
---
+++ src/hooks-store/store.js
@@ -0,0 +1,41 @@
+//managing state globally with react
+import { useState, useEffect } from "react";
+
+let globalState = {}; 
+let listeners = []; //to listen to component changes across the app
+//listener -> array of functions -> updates to global state
+let actions = {}; 
+
+//create a custome hook 
+export const useStore = () => {
+    const setStateValue = useState(globalState)[1] //makes components to re render 
+
+    const dispatch = (actionId, payload) =>{ // payload = prod id 
+     const newstate =  actions[actionId](globalState, payload) //return a new state
+     globalState = {...globalState, ...newstate}
+      
+        for (const listener of listeners){
+            listener(globalState) //this will re render the component that is changing its state
+        }
+    }
+
+    useEffect(() => {
+        listeners.push(setStateValue);
+
+    return () => {
+
+        listeners = listeners.filter(list => list !== setStateValue)
+
+    }//this is ran whenever the component is removed from the dom 
+    }, [setStateValue]) //only runs for the component that uses the hook when the component renders 
+
+    return [globalState, dispatch]
+}
+
+export const initStore = (userActions, initialState) => {
+    if(initialState){
+        globalState = {...globalState, ...initialState}
+    }
+
+    actions = {...actions, ...userActions}
+}
